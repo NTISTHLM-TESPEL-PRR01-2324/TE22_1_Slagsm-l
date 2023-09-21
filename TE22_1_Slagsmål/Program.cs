4ce6b9c4970260fb03bb0cb75f291f2a7f08c54e
Mikael Bergström <mikael.bergstrom@ga.ntig.se>
2023-09-21 10:54:22 +0200

    syj

--- TE22_1_Slagsmål/Program.cs
+++ TE22_1_Slagsmål/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// SLUMP
+// Random generator = new Random();
+
+// int slumptal = generator.Next(10, 20);
+
+// Console.WriteLine(slumptal);
+
+
+// WHILE-LOOP
+
+
+int tal = 100;
+
+while (tal > 0)
+{
+  Console.WriteLine(tal);
+  tal--;
+}
+
+Console.WriteLine("FÄRDIG");
+
+Console.ReadLine();
